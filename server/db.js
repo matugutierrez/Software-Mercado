43ea99506058e94edd1a9d9a07200fa604b90050
Matu gutierrez <matugutierrez444@gmail.com>
2026-07-06 19:54:13 -0300

    fix movilessss

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg')
 
 const useSsl = process.env.DATABASE_SSL === 'true'
+const timezone = process.env.TZ || 'America/Argentina/Buenos_Aires'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -8,7 +9,13 @@ const pool = new Pool({
 })
 
 async function query(text, params) {
-  return pool.query(text, params)
+  const client = await pool.connect()
+  try {
+    await client.query(`SET TIMEZONE TO '${timezone}'`)
+    return await client.query(text, params)
+  } finally {
+    client.release()
+  }
 }
 
 module.exports = { pool, query }
